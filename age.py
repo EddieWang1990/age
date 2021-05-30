2bc4d55c5252ef3a19a699681eacd2fe952dce36
EddieWang1990 <84988244+EddieWang1990@users.noreply.github.com>
2021-05-31 00:39:07 +0800

    finished version

---
+++ age.py
@@ -0,0 +1,13 @@
+driving = input('請問你有沒有開過車? ')
+age = input('請問你的年齡? ')
+age = int(age)
+if driving == '有':
+	if age >= 18:
+		print('通過測驗了')
+	else:
+		print('奇怪 你怎麼會有開過車')
+else:
+	if age >= 18:
+		print('你可以去考駕照')
+	else:
+		print('等你18歲就可以去考駕照')
